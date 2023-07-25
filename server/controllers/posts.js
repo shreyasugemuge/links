3ae86567b6e81ebee0d7672a4b95d095965d0feb
Shreyas Ugemuge <uge@Shreyass-MacBook-Pro.local>
2023-07-25 16:58:25 +0530

    asap

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -23,7 +23,7 @@ export const createPost = async (req, res) => {
     } catch (err) {
         res.status(409).json({ message: err.message });
     }
-
+}
 //read
 export const getFeedPosts = async (req, res) => {
     try {
@@ -46,26 +46,26 @@ export const getUserPosts = async (req, res) => {
 
 //update
 export const likePost = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { userId } = req.body;
-        const post = await Post.findById(id);
-        const isLiked = post.likes.get(userId);
+        try {
+            const { id } = req.params;
+            const { userId } = req.body;
+            const post = await Post.findById(id);
+            const isLiked = post.likes.get(userId);
 
-        if (isLiked) {
-            post.likes.delete(userId);
-        } else {
-            post.likes.set(userId, true);
+            if (isLiked) {
+                post.likes.delete(userId);
+            } else {
+                post.likes.set(userId, true);
+            }
+
+            const updatedPost = await Post.findByIdAndUpdate(
+                id,
+                { likes: post.likes },
+                { new: true }
+            );
+
+            res.status(200).json(updatedPost);
+        } catch (err) {
+            res.status(404).json({ message: err.message });
         }
-
-        const updatedPost = await Post.findByIdAndUpdate(
-            id,
-            { likes: post.likes },
-            { new: true }
-        );
-        
-        res.status(200).json(updatedPost);
-    } catch (err) {
-        res.status(404).json({ message: err.message });
     }
-}
