2cd0d16c4d0531633a113c881068bd7f8fd9b3e7
Shreyas Ugemuge <uge@Shreyass-MacBook-Pro.local>
2023-07-28 15:10:34 +0530

    mm2

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -40,7 +40,7 @@ const PostWidget = ({
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-    const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+    const response = await fetch(`http://34.93.80.151:3001/posts/${postId}/like`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
